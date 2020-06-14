f268fa62810881f748dc16d1d9fb3a277d9ecb5a
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-14 16:13:21 -0400

    stage

--- src/bellal420/Test420.java
+++ src/bellal420/Test420.java
@@ -1,10 +1,14 @@
 package bellal420;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test420 {
 
 	public static void main(String[] args) {
-		
-		
+
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
 
 	}
 
